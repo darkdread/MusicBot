1bb8132fb60387e2c8949f31bde9242387b72987
Clarence <arclucifer4@gmail.com>
2017-11-03 11:48:32 +0800

    image search LOOOOOOOOOOOOOOOOOOOOL 4HEad

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -11,6 +11,8 @@ import traceback
 import requests
 import atexit
 from git import Repo
+from urllib.request import urlopen
+import lxml.html
 
 from discord import utils
 from discord.object import Object
@@ -779,6 +781,17 @@ class MusicBot(discord.Client):
 
             return Response(helpmsg, reply=True, delete_after=60)
 	
+    async def cmd_image(self, message):
+        """
+        Usage:
+            {command_prefix}image [title]
+
+        Displays the first image from Google search.
+        """
+        html = lxml.html.parse(urlopen("https://images.google.com")).getroot()
+        print(html.text_content())
+        return Response("My name is Clarence Goh and I am an engineer from Singapore Polytechnic.", delete_after=20)
+
     async def cmd_clarencegoh(self):
         """
         Usage:
